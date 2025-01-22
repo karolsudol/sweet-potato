1ffe3eeb84fe801d8d52590c59cb774109495fd4
Karol Sudol <karol@sudoltrust.com>
2025-01-22 11:51:21 +0000

    sql cleanup

--- query_receipts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Load environment variables
-source .env
-
-# Execute query
-curl "http://localhost:8123/?user=$CLICKHOUSE_USER&password=$CLICKHOUSE_PASSWORD&query=SELECT%20transaction_hash,block_number,datetime,status,gas_used%20FROM%20sweet_potatoe_dbt.raw_receipts%20ORDER%20BY%20datetime%20DESC%20LIMIT%205%20FORMAT%20Pretty" 
